5f8759146241aa9fec7f56114f422f164e3712d9
Mariam Heikal <mariamheikal228@gmail.com>
2020-08-23 20:16:45 +0200

    language headrer added

--- samples/player/getPlayerInfo.js
+++ samples/player/getPlayerInfo.js
@@ -1,12 +1,12 @@
 var gameball = require('../../lib/gameball')
-var Gameball = new gameball('7c7636658209418c9a82306a421f76a5')
+var Gameball = new gameball('your_api_key')
 Gameball.setUp({
     'transactionKey':'your_transaction_key'
 });
 
 
 Gameball.getPlayerInfo({
-    "playerUniqueId":'player_unique_id'
+    "playerUniqueId":'palyer_unique_id'
 },function (err, res){
     if(err) console.log(err)
     else console.log(res)
